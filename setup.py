12d98b8992df2813ecd29c113af11e13f3758fb6
Nat Wilson <njwilson23@gmail.com>
2016-12-28 20:48:33 -0800

    add setup script and tests

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="picogeojson",
+        version="0.1.0",
+        description="A minimal geojson serializer and deserializer",
+        author="Nat Wilson",
+        author_email="natw@fortyninemaps.com",
+        license="MIT",
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+
+            # Indicate who your project is intended for
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+
+            # Pick your license as you wish (should match "license" above)
+            'License :: OSI Approved :: MIT License',
+
+            # Specify the Python versions you support here. In particular, ensure
+            # that you indicate whether you support Python 2, Python 3 or both.
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6'
+            ],
+        keywords="minimal geojson",
+        packages=find_packages(exclude=["tests"])
+)
---
+++ tests/tests.py
@@ -0,0 +1,149 @@
+""" Unit tests for vector functions """
+
+import unittest
+import os
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+import json
+
+import picogeojson as pgj
+from picogeojson import Serializer, Deserializer, DEFAULTCRS
+
+TESTDATA = "tests/"
+
+class DeserializerTests(unittest.TestCase):
+
+    def test_point_read(self):
+        with open(os.path.join(TESTDATA, 'point.json')) as f:
+            reader = Deserializer(f)
+        res = reader.parse()
+        self.assertEqual(res.coordinates, [100.0, 0.0])
+        return
+
+    def test_linestring_read(self):
+        with open(os.path.join(TESTDATA, 'linestring.json')) as f:
+            reader = Deserializer(f)
+        res = reader.parse()
+        self.assertEqual(res.coordinates, [[100.0, 0.0], [101.0, 1.0]])
+        return
+
+    def test_polygon_read(self):
+        with open(os.path.join(TESTDATA, 'polygon.json')) as f:
+            reader = Deserializer(f)
+        res = reader.parse()
+        self.assertEqual(res.coordinates,
+            [[[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]]])
+        return
+
+    def test_multipoint_read(self):
+        with open(os.path.join(TESTDATA, 'multipoint.json')) as f:
+            reader = Deserializer(f)
+        res = reader.parse()
+        self.assertEqual(res.coordinates, [[100.0, 0.0], [101.0, 1.0]])
+        return
+
+    def test_multilinestring_read(self):
+        with open(os.path.join(TESTDATA, 'multilinestring.json')) as f:
+            reader = Deserializer(f)
+        res = reader.parse()
+        self.assertEqual(res.coordinates, [[[100.0, 0.0], [101.0, 1.0]],
+                                              [[102.0, 2.0], [103.0, 3.0]]])
+        return
+
+    def test_multipolygon_read(self):
+        with open(os.path.join(TESTDATA, 'multipolygon.json')) as f:
+            reader = Deserializer(f)
+        res = reader.parse()
+        self.assertEqual(res.coordinates,
+            [[[[102.0, 2.0], [103.0, 2.0], [103.0, 3.0], [102.0, 3.0], [102.0, 2.0]]],
+             [[[100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0]],
+             [[100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2]]]])
+        return
+
+    def test_featurecollection_read(self):
+        path = os.path.join(TESTDATA, "featurecollection.json")
+        with open(path) as f:
+            reader = Deserializer(f)
+        fc = reader.parse()
+        self.assertTrue(isinstance(fc.features[0].geometry, pgj.Point))
+        self.assertEqual(fc.features[0].geometry.coordinates, [102.0, 0.5])
+        self.assertEqual(fc.features[0].properties["scalar"], {"prop0": "value0"})
+
+        self.assertTrue(isinstance(fc.features[1].geometry, pgj.LineString))
+        self.assertEqual(fc.features[1].geometry.coordinates,
+                        [[102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]])
+        self.assertEqual(fc.features[1].properties["scalar"],
+                        {"prop0": "value0", "prop1": 0.0})
+
+        self.assertTrue(isinstance(fc.features[2].geometry, pgj.Polygon))
+        self.assertEqual(fc.features[2].geometry.coordinates,
+                        [[[100.0, 0.0], [101.0, 0.0], [101.0, 1.0],
+                          [100.0, 1.0], [100.0, 0.0]]])
+        self.assertEqual(fc.features[2].properties["scalar"],
+                        {"prop0": "value0", "prop1": {"this": "that"}})
+        return
+
+class SerializerTests(unittest.TestCase):
+
+    def setUp(self):
+        self.serializer = Serializer()
+        return
+
+    def test_serialize_point(self):
+        pt = pgj.Point((44.0, 17.0), DEFAULTCRS)
+        s = self.serializer(pt)
+        d = json.loads(s)
+        self.assertEqual(tuple(pt.coordinates), tuple(d["coordinates"]))
+        return
+
+    def test_serialize_linestring(self):
+        linestring = pgj.LineString([[44.0, 17.0], [43.0, 17.5], [-2.1, 4.0]],
+                                        DEFAULTCRS)
+        s = self.serializer(linestring)
+        d = json.loads(s)
+        self.assertEqual(list(linestring.coordinates), list(d["coordinates"]))
+        return
+
+    def test_serialize_polygon(self):
+        polygon = pgj.Polygon([[[44.0, 17.0], [43.0, 17.5], [-2.1, 4.0], [44.0, 17.0]],
+                                   [[1.0, 1.0], [0.5, -0.5], [0.8, -0.7], [1.0, 1.0]]],
+                                  DEFAULTCRS)
+        s = self.serializer(polygon)
+        d = json.loads(s)
+
+        self.assertEqual(list(polygon.coordinates), list(d["coordinates"]))
+        return
+
+    def test_serialize_multipoint(self):
+        multipoint = pgj.MultiPoint([[44.0, 17.0], [43.0, 17.5], [-2.1, 4.0]],
+                                        DEFAULTCRS)
+        s = self.serializer(multipoint)
+        d = json.loads(s)
+        self.assertEqual(list(multipoint.coordinates), list(d["coordinates"]))
+        return
+
+    def test_serialize_multilinestring(self):
+        multilinestring = pgj.MultiLineString(
+                            [[[44.0, 17.0], [43.0, 17.5], [-2.1, 4.0]],
+                             [[49.0, -3.0], [48.0, -2.5], [2.9, -16.0]]],
+                            DEFAULTCRS)
+        s = self.serializer(multilinestring)
+        d = json.loads(s)
+        self.assertEqual(list(multilinestring.coordinates), list(d["coordinates"]))
+        return
+
+    def test_serialize_multipolygon(self):
+        multipolygon = pgj.MultiPolygon(
+                            [[[44.0, 17.0], [43.0, 17.5], [-2.1, 4.0]],
+                              [[1.0, 1.0], [0.5, -0.5], [0.8, [-0.7]]],
+                             [[[49.0, -3.0], [48.0, -2.5], [2.9, -16.0]]]],
+                            DEFAULTCRS)
+        s = self.serializer(multipolygon)
+        d = json.loads(s)
+        self.assertEqual(list(multipolygon.coordinates), list(d["coordinates"]))
+        return
+
+if __name__ == "__main__":
+    unittest.main()
