1c881d86293e645fa1925cf4eb92e46b2b15af05
Nat Wilson <njwilson23@gmail.com>
2017-05-02 19:18:01 -0700

    patch for PyPy raising a different error type

--- picogeojson/geojson.py
+++ picogeojson/geojson.py
@@ -47,8 +47,10 @@ def docstring_insert(*s):
     def wrapped(obj):
         try:
             obj.__doc__ = obj.__doc__.format(*s)
-        except AttributeError:
-            pass # class __doc__ attribute not writable in Python 2
+        except (AttributeError, TypeError):
+            # class __doc__ attribute not writable in Python 2
+            # and in PyPy an TypeError is raised rather than an AttributeError
+            pass
         return obj
     return wrapped
 
